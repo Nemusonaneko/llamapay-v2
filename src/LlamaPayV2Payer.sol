4f938c0a56e3916a3d4d9e682f73c7256824eb13
nemusonaneko <nemusonaneko@protonmail.com>
2022-06-25 03:03:18 -0500

    withdrawPayer and should track vault tokens instead of underlying tokens lol

--- src/LlamaPayV2Payer.sol
+++ src/LlamaPayV2Payer.sol
@@ -46,32 +46,32 @@ contract LlamaPayV2Payer {
         token.approve(_vaultToken, _amount);
         vaultToken.deposit(_amount, address(this));
         uint8 decimals = token.decimals();
-        tokens[ _token].balance += _amount * (10 ** (20 - decimals));
+        tokens[_vaultToken].balance += _amount * (10 ** (20 - decimals));
     }
 
     function withdraw(uint _id, uint _amount) public {
         address payee = ERC721(factory).ownerOf(_id);
         require(payee != address(0), "stream already burned");
         Stream storage stream = streams[_id];
-        Token storage token = tokens[stream.token];
+        Token storage token = tokens[stream.vaultToken];
         ERC20 assetToken = ERC20(stream.token);
         ERC4626 vaultToken = ERC4626(stream.vaultToken);
 
         // Deduct payer balance since last update
         uint paidSinceUpdate = (block.timestamp - token.lastPayerUpdate) * token.totalPaidPerSec;
         if (token.balance >= paidSinceUpdate) {
-            tokens[stream.token].balance -= paidSinceUpdate;
-            tokens[stream.token].lastPayerUpdate = uint40(block.timestamp);
+            tokens[stream.vaultToken].balance -= paidSinceUpdate;
+            tokens[stream.vaultToken].lastPayerUpdate = uint40(block.timestamp);
         } else {
             uint timePaid = token.balance / token.totalPaidPerSec;
-            tokens[stream.token].lastPayerUpdate += uint40(timePaid);
-            tokens[stream.token].balance = token.balance % token.totalPaidPerSec;
+            tokens[stream.vaultToken].lastPayerUpdate += uint40(timePaid);
+            tokens[stream.vaultToken].balance = token.balance % token.totalPaidPerSec;
         }
 
         // Update stream
         uint available = (token.lastPayerUpdate - stream.lastUpdate) * stream.amountPerSec;
         require(available >= _amount, "amount > balance");
-        tokens[stream.token].balance -= _amount;
+        tokens[stream.vaultToken].balance -= _amount;
         streams[_id].lastUpdate += uint40(_amount / stream.amountPerSec);
 
         // Redeem from pool 
@@ -85,7 +85,7 @@ contract LlamaPayV2Payer {
         require(transferred, "transfer to payee failed");
         assetToken.approve(stream.vaultToken, split);
         vaultToken.deposit(split, address(this));
-        tokens[stream.token].balance += split * (10 ** (20 - decimals));
+        tokens[stream.vaultToken].balance += split * (10 ** (20 - decimals));
     }
 
     function createStream(address _token, address _vaultToken, address _payee, uint216 _amountPerSec) external {
@@ -97,10 +97,10 @@ contract LlamaPayV2Payer {
         require(_amountPerSec > 0, "amount per sec cannot be 0");
         
         // Update token info
-        uint delta = block.timestamp - tokens[_token].lastPayerUpdate;
-        tokens[_token].balance -= delta * tokens[_token].totalPaidPerSec;
-        tokens[_token].totalPaidPerSec += _amountPerSec;
-        tokens[_token].lastPayerUpdate = uint40(block.timestamp);
+        uint delta = block.timestamp - tokens[_vaultToken].lastPayerUpdate;
+        tokens[_vaultToken].balance -= delta * tokens[ _vaultToken].totalPaidPerSec;
+        tokens[_vaultToken].totalPaidPerSec += _amountPerSec;
+        tokens[_vaultToken].lastPayerUpdate = uint40(block.timestamp);
 
         Factory nftFactory = Factory(factory);
 
@@ -125,7 +125,7 @@ contract LlamaPayV2Payer {
         Factory nftFactory = Factory(factory);
         (bool burned) = nftFactory.burn(_id);
         require(burned, "failed to burn nft");
-        tokens[streams[_id].token].totalPaidPerSec -= streams[_id].amountPerSec;
+        tokens[streams[_id].vaultToken].totalPaidPerSec -= streams[_id].amountPerSec;
     }
 
     function modifyStream(uint _id, address _token, address _vaultToken, uint216 _amountPerSec) external {
@@ -138,12 +138,12 @@ contract LlamaPayV2Payer {
         // Withdraw and basically cancel the current stream
         (uint withdrawableAmount) = withdrawable(_id);
         withdraw(_id, withdrawableAmount);
-        tokens[streams[_id].token].totalPaidPerSec -= streams[_id].amountPerSec;
+        tokens[streams[_id].vaultToken].totalPaidPerSec -= streams[_id].amountPerSec;
         // Create a new stream
-        uint delta = block.timestamp - tokens[_token].lastPayerUpdate;
-        tokens[_token].balance -= delta * tokens[_token].totalPaidPerSec;
-        tokens[_token].totalPaidPerSec += _amountPerSec;
-        tokens[_token].lastPayerUpdate = uint40(block.timestamp);
+        uint delta = block.timestamp - tokens[_vaultToken].lastPayerUpdate;
+        tokens[_vaultToken].balance -= delta * tokens[_vaultToken].totalPaidPerSec;
+        tokens[_vaultToken].totalPaidPerSec += _amountPerSec;
+        tokens[_vaultToken].lastPayerUpdate = uint40(block.timestamp);
         // Update current stream token
         streams[_id] = Stream({
             amountPerSec: _amountPerSec,
@@ -151,6 +151,18 @@ contract LlamaPayV2Payer {
             token: _token,
             vaultToken: _vaultToken
         });
+    }
+
+    function withdrawPayer(address _token, address _vaultToken, uint _amount) external {
+        ERC20 assetToken = ERC20(_token);
+        ERC4626 vaultToken = ERC4626(_vaultToken);
+        require(msg.sender == owner, "not owner");
+        tokens[_vaultToken].balance -= _amount;
+        uint delta = block.timestamp - tokens[_vaultToken].lastPayerUpdate;
+        require(tokens[_vaultToken].balance >= delta * tokens[_vaultToken].totalPaidPerSec, "amount > balance");
+        uint8 decimals = assetToken.decimals();
+        uint toRedeem = _amount / (10 ** (20 - decimals));
+        vaultToken.redeem(toRedeem, owner, address(this));
     }
 
     function transferOwnership(address _futureOwner) external {
@@ -165,7 +177,7 @@ contract LlamaPayV2Payer {
 
     function withdrawable(uint _id) public view returns (uint withdrawableAmount) {
         Stream storage stream = streams[_id];
-        Token storage token = tokens[stream.token];
+        Token storage token = tokens[stream.vaultToken];
         uint paidSinceUpdate = (block.timestamp - token.lastPayerUpdate) * token.totalPaidPerSec;
         uint lastUpdate;
         if (token.balance >= paidSinceUpdate) {
@@ -175,6 +187,7 @@ contract LlamaPayV2Payer {
         }
         withdrawableAmount = ((lastUpdate - stream.lastUpdate) * stream.amountPerSec);
     }
+
 
 
 }
